dc7c0e4d9bb158f10b5d4d9c09af6dea6a836689
Adam <Adam@sigterm.info>
2021-09-16 13:36:36 -0400

    Add macos aarch64 support

---
+++ build-osx-aarch64.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e
+
+JDK_VER="11.0.8"
+JDK_BUILD="10"
+PACKR_VERSION="runelite-1.0"
+
+SIGNING_IDENTITY="Developer ID Application"
+ALTOOL_USER="user@icloud.com"
+ALTOOL_PASS="@keychain:altool-password"
+
+FILE=OpenJDK17-jre_aarch64_mac_hotspot_2021-09-14-09-41.tar.gz
+URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk17-2021-09-14-09-41-beta/OpenJDK17-jre_aarch64_mac_hotspot_2021-09-14-09-41.tar.gz
+
+if ! [ -f $FILE ] ; then
+	curl -Lo $FILE $URL
+fi
+#if ! [ -f OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz ] ; then
+#    curl -Lo OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz \
+#        https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
+#fi
+
+#echo "b0cd349e7e428721a3bcfec619e071d25c0397e3e43b7ce22acfd7d834a8ca4b  OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | shasum -c
+
+
+# packr requires a "jdk" and pulls the jre from it - so we have to place it inside
+# the jdk folder at jre/
+if ! [ -d osx-aarch64-jdk ] ; then
+    tar zxf $FILE
+    mkdir osx-aarch64-jdk
+    mv jdk-17+35-jre osx-aarch64-jdk/jre
+    #tar zxf OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
+    #mkdir osx-jdk
+    #mv jdk-${JDK_VER}+${JDK_BUILD}-jre osx-jdk/jre
+
+    pushd osx-aarch64-jdk/jre
+    # Move JRE out of Contents/Home/
+    mv Contents/Home/* .
+    # Remove unused leftover folders
+    rm -rf Contents
+    popd
+fi
+
+#if ! [ -f packr_${PACKR_VERSION}.jar ] ; then
+#    curl -Lo packr_${PACKR_VERSION}.jar \
+#        https://github.com/runelite/packr/releases/download/${PACKR_VERSION}/packr.jar
+#fi
+
+#echo "18b7cbaab4c3f9ea556f621ca42fbd0dc745a4d11e2a08f496e2c3196580cd53  packr_${PACKR_VERSION}.jar" | shasum -c
+
+#java -jar packr_${PACKR_VERSION}.jar
+java -jar packr-2.1-SNAPSHOT-jar-with-dependencies.jar \
+	macos-aarch64-config.json
+
+cp target/filtered-resources/Info.plist native-osx-aarch64/RuneLite.app/Contents
+
+echo Setting world execute permissions on RuneLite
+pushd native-osx-aarch64/RuneLite.app
+chmod g+x,o+x Contents/MacOS/RuneLite
+popd
+
+#codesign -f -s "${SIGNING_IDENTITY}" --entitlements osx/signing.entitlements --options runtime native-osx-aarch64/RuneLite.app || true
+
+# create-dmg exits with an error code due to no code signing, but is still okay
+create-dmg native-osx-aarch64/RuneLite.app native-osx-aarch64/ || true
+
+mv native-osx-aarch64/RuneLite\ *.dmg RuneLite-aarch64.dmg
+
+#xcrun altool --notarize-app --username "${ALTOOL_USER}" --password "${ALTOOL_PASS}" --primary-bundle-id runelite --file RuneLite-aarch64.dmg || true
